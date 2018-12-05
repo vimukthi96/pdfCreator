0eb1b45adfe424d20bd4587d72f2bc324da0e223
vimukthi96 <vimukthi072@gmail.com>
2018-12-05 14:44:09 +0530

    change code

--- app/src/main/java/com/example/vimukthi/pdfcretor/MainActivity.java
+++ app/src/main/java/com/example/vimukthi/pdfcretor/MainActivity.java
@@ -14,35 +14,31 @@ import android.util.Log;
 import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.DocumentException;
-import com.lowagie.text.Font;
-import com.lowagie.text.HeaderFooter;
-import com.lowagie.text.Image;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.Phrase;
-import com.lowagie.text.pdf.PdfWriter;
-
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnCreatePDF,btnViewPDF;
+   /* private Button btnCreatePDF,btnViewPDF;
     public static final int REQUEST_PERM_WRITE_STORAGE =102;
-    public static final int REQUEST_PERM_READ_STORAGE =103;
+    public static final int REQUEST_PERM_READ_STORAGE =103;*/
 
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCreatePDF=(Button)findViewById(R.id.btnCreatePdf);
+      /*  btnCreatePDF=(Button)findViewById(R.id.btnCreatePdf);
         btnViewPDF=(Button)findViewById(R.id.btnViewPdf);
 
         btnCreatePDF.setOnClickListener(new View.OnClickListener() {
@@ -73,10 +69,10 @@ public class MainActivity extends AppCompatActivity {
                 }
                // viewPDF();
             }
-        });
-    }
+        });*/
 
-    private void viewPDF() {
+
+   /* private void viewPDF() {
 
         String path =Environment.getExternalStorageDirectory().getAbsolutePath() + "/PDF/demo.pdf";
         Intent intent =new Intent(Intent.ACTION_VIEW);
@@ -113,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
             p1.setAlignment(Paragraph.ALIGN_CENTER);
             p1.setFont(paraFont);
 
-            document.add(p1);
+            p1.add();
 
             Paragraph p2 =new Paragraph("sjdhgsadhjksld dgdhjwkqdkjhgjhfgd  wydwfgdhjwdjh");
 
@@ -146,9 +142,82 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (IOException e){
             Log.e("PDFCReator","IOException:" +e);
-        }
-        finally {
+        } catch (BadElementException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } finally {
             document.close();
         }
     }
+}*/
+   private static final String FILE_NAME = "example.pdf";
+
+    EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText = findViewById(R.id.edit_text);
+    }
+
+    public void save(View v) {
+        String text = mEditText.getText().toString();
+        FileOutputStream fos = null;
+
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+
+            mEditText.getText().clear();
+            Toast.makeText(this, "Saved to " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void load(View v) {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+
+            mEditText.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
+
